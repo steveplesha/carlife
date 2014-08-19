3a0f63dfa4b9e3a0a8eb1055cec1486be326505c
Steve Plesha <steveplesha@gmnail.com>
2014-08-18 23:12:54 -0500

    Added angualar code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+(function() {
+    var app = angular.module('myApp', []);
+    
+    app.controller('CarController', function(){
+        this.cars = vehicle;
+    });
+    
+    var vehicle = [
+        {
+            name: 'Corolla',
+            type: "sedan",
+            mileage: 90000
+        },
+        {
+            name: "Grand Caravan",
+            type: "van",
+            mileage: 40000
+        }];
+})();
