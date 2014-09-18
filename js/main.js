6fc3c4ca1ac8c6b90943c661342ed11f6bd95f44
Steve Plesha <steveplesha@gmnail.com>
2014-09-17 22:20:18 -0500

    added footer partial, added car repairs array

--- js/main.js
+++ js/main.js
@@ -21,11 +21,20 @@
         {
             name: 'Corolla',
             type: "Sedan",
-            mileage: 90000
+            mileage: 90000,
+            picture: '/img/2006ToyotaCorolla.jpg',
+            repairs: [
+                {
+                    workDone: "Oil Change",
+                    cost: 35,
+                    shop: "Duke of Oil"
+                }
+            ]
         },
         {
             name: "Grand Caravan",
             type: "Van",
-            mileage: 40000
+            mileage: 40000,
+            picture: '/img/2012GrandCaravan.jpg'
         }];
 })();
