e90868fc86b5e3e759e5b198ff8c47173341d01f
Steve Plesha <steveplesha@gmnail.com>
2014-10-01 22:50:14 -0500

    removed directive, added push to firebase

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,25 @@
 (function() {
     var app = angular.module('myApp', []);
     var myDataRef = new Firebase('https://burning-heat-392.firebaseio.com');
+        
+    app.controller("CarController", function() {
+        this.cars = vehicle;
+    });
     
-    app.directive('carList', [ '$http', function($http) {
-        return {
-            restrict: 'E',
-            templateUrl: 'car-info.html'//,
-            //controller: function(){
-            //    var inventory = this;
-            //    inventory.cars = [];
-            //    $http.get('/car.json').success(function(data){
-            //        inventory.cars = data;
-            //    });
-            //},
-            //controllerAs: 'car'
-        };
-    }]);
-    
-    app.controller("CarController", [ '$http', function($http) {
-        var inventory = this;
-        inventory = [ ];
-        $http.get('/js/car.json').success(function(data){
-            inventory.cars=data;
+    var carRef = myDataRef.child('vehicles');
+    carRef.push({
+        name: 'Corolla',
+            type: "Sedan",
+            mileage: 90000,
+            picture: 'app/img/2006ToyotaCorolla.jpg',
+            repairs: [
+                {
+                    workDone: "Oil Change",
+                    cost: 35,
+                    shop: "Duke of Oil"
+                }
+            ]
         });
-    }]);
     
     var vehicle = [
         {
