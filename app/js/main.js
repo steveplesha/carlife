fa13d088e729be1131b66b6dd5c2b6aee42252f7
steveplesha <steveplesha@gmail.com>
2014-12-19 15:15:29 -0600

    Started working on image upload

--- app/js/main.js
+++ app/js/main.js
@@ -177,7 +177,7 @@
     app.controller("EditCarController", ["$scope", "$firebase", "$location", "$routeParams", function($scope, $firebase, $location, $routeParams) {
         console.log("starting edit controller");
         var authData = myDataRef.getAuth();
-        var ref = new Firebase(myDataRef + 'users/' + authData.uid + '/vehicles');
+        var ref = new Firebase(myDataRef + '/users/' + authData.uid + '/vehicles');
         var vehicleRef = myDataRef.child('users').child(authData.uid).child('vehicles')
         var sync = $firebase(ref);
         var id = $routeParams.id;
@@ -214,55 +214,5 @@
             $location.path('/cars');
         };
     }]);
-            
-    function handleFileSelect(evt) {
-        var f = evt.target.files[0];
-        var reader = new FileReader();
-        console.log("entered file select");
-        reader.onload = (function(theFile) {
-            return function(e) {
-                var filePayload = e.target.result;
-                // Generate a location that can't be guessed using the file's contents and a random number
-                var hash = CryptoJS.SHA256(Math.random() + CryptoJS.SHA256(filePayload));
-                var f = new Firebase(myDataRef + '/pano/' + hash + '/filePayload');
-                console.log(f);
-               // spinner.spin(document.getElementById('spin'));
-               // Set the file payload to Firebase and register an onComplete handler to stop the spinner and show the preview
-               f.set(filePayload, function() { 
-                 // spinner.stop();
-                            //document.getElementById("pano").src = e.target.result;
-                 //$('#file-upload').hide();
-                 // Update the location bar so the URL can be shared with others
-                 window.location.hash = hash;
-               });
-             };
-            })(f);
-            reader.readAsDataURL(f);
-        }        
-    /*$(function() {
-        // $('#spin').append(spinner);
-        console.log("entered function");
-        var myDataRef = new Firebase('https://burning-heat-392.firebaseio.com');
-        var idx = window.location.href.indexOf('#');
-        var hash = (idx > 0) ? window.location.href.slice(idx + 1) : '';
-        if (hash === '') {
-            // No hash found, so render the file upload button.
-            $('#file-upload').show();
-            //document.getElementById("file-upload").addEventListener('change', handleFileSelect, false);
-        } else {
-            // A hash was passed in, so let's retrieve and render it.
-            // spinner.spin(document.getElementById('spin'));
-            var f = new Firebase(myDataRef + '/pano/' + hash + '/filePayload');
-            console.log(f);
-        f.once('value', function(snap) {
-            var payload = snap.val();
-            if (payload != null) {
-            document.getElementById("pano").src = payload;
-            } else {
-            $('#body').append("Not found");
-            }
-            //spinner.stop();
-        });
-      }
-    }); */ 
+
 }())
